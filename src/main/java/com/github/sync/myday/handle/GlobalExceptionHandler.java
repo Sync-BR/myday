8d845fb752f3f24fde5d53e42a99721db9474883
Eduardo <eduardofreitasfr@gmail.com>
2025-10-20 23:02:37 -0300

    feat: controle de exception em diferentes erros

--- src/main/java/com/github/sync/myday/handle/GlobalExceptionHandler.java
+++ src/main/java/com/github/sync/myday/handle/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.github.sync.myday.handle;
 
+import com.github.sync.myday.handle.exception.InvalidTokenException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.security.SignatureException;
 
 import java.util.*;
 
@@ -29,4 +35,49 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.badRequest().body(body);
     }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<Map<String, Object>> handleMissingRequestHeader(MissingRequestHeaderException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("timestamp", new Date());
+        body.put("error", "Header obrigatório não informado");
+
+        return ResponseEntity.status(400).body(body);
+    }
+
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<Map<String, Object>> handleExpiredJwtException(ExpiredJwtException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("timestamp", new Date());
+        body.put("error", "Token expirado");
+        body.put("details", "O token JWT expirou. Faça login novamente para obter um novo token");
+
+        return ResponseEntity.status(401).body(body);
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<Map<String, Object>> handleSignatureException(SignatureException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("timestamp", new Date());
+        body.put("error", "Assinatura do token inválida");
+        body.put("details", "A assinatura do token JWT é inválida. O token pode ter sido alterado");
+
+        return ResponseEntity.status(401).body(body);
+    }
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidTokenException(InvalidTokenException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("timestamp", new Date());
+        body.put("error", "Token de usuário invalido, tente novamente");
+        return ResponseEntity.status(401).body(body);
+
+    }
+
+
+
 }
